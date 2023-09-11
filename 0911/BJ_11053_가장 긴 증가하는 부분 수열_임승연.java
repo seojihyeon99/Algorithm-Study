e29cc135516605091e76eb6a92ff77ae9f7e95aa
seungyeon lim <91115868+sye0ni@users.noreply.github.com>
2023-09-11 23:33:24 +0900

    Create BJ_11053_가장 긴 증가하는 부분 수열_임승연.java

---
+++ 0911/BJ_11053_가장 긴 증가하는 부분 수열_임승연.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * dp[i]: i번째 수까지의 최장중가부분수열크기 저장
+ * 메모리 12360 kb, 시간 96 ms
+ */
+
+public class bj_11053 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        int[] arr=new int[n];
+        int[] dp=new int[n];
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<n;i++) arr[i]=Integer.parseInt(st.nextToken());
+        dp[0]=1;
+
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){ // 내 앞을 봄
+                if(arr[j]<arr[i]){
+                    if(dp[j]>=dp[i]) dp[i]=dp[j]+1; // 최장 길이 찾기
+                }
+            }
+            if(dp[i]==0) dp[i]=1; // 나보다 작은 값이 없다면 1로 set
+        }
+
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]>max) max=dp[i];
+        } // 최댓값 찾기
+        System.out.println(max);
+    }
+}
